797ca6b2066d71621daf6e761b4b4b4d81b092bb
NabirasulA <nabirasula2004@gmail.com>
2026-01-31 14:05:51 +0530

    Add Spring Boot project structure

---
+++ src/main/java/com/project/porfoliomanagement/PorfoliomanagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.porfoliomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorfoliomanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PorfoliomanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/porfoliomanagement/PorfoliomanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.porfoliomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorfoliomanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
